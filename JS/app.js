1aa7db4a0cdac095089d6a6157e28d641ecaadfb
samahthwib <samah.thweib@hotmail.com>
2020-02-18 21:35:15 +0200

    added files

--- JS/app.js
+++ JS/app.js
@@ -1,70 +1,154 @@
 'use strict';
 
 
-var yourName = prompt('Whats your name ? ');
-alert('Welcome to my website ' + yourName);
+// var yourName = prompt('Whats your name ? ');
+// alert('Welcome to my website ' + yourName);
 //console.log('Welcome to my website '+ yourName);
 
-
+var score=0;
 var myName = prompt('Do you know my name ? ').toUpperCase();
 //console.log(myName);
 if (myName === 'YES' || myName === 'Y') {
-  alert(myName + ' You Know me! much love ' );
+// alert(myName + ' You Know me!  ' );
+  score++;
 } else if (myName === 'NO' || myName === 'N') {
-  alert(myName + ' You Do not know me , but now you will know me ');
+// alert(myName + ' You Don\'t know me , but now you will know me later');
 } else {
   alert('Nothing');
 }
 
-var programmingLove= prompt(' Do you like programming? ').toUpperCase();
-//console.log(programmingLove);
-if (programmingLove === 'YES' || programmingLove === 'Y') {
-  alert(programmingLove +' Me too! and this is what i am studying at this time' );
-} else if (programmingLove === 'NO' || programmingLove === 'N') {
-  alert(programmingLove + ' If you learn it you will love it ');
+var bachelorsMajor= prompt(' Do you know what is my major in the university? ').toUpperCase();
+//console.log(bachelorsDegree);
+if (bachelorsMajor === 'YES' || bachelorsMajor === 'Y') {
+// alert(bachelorsDegree +'You are right' );
+  score++;
+} else if (bachelorsMajor === 'NO' || bachelorsMajor === 'N') {
+//alert(bachelorsDegree + ' you will know it later ');
 } else{
   alert('Nothing');
 }
 
-var loveLearnign = prompt('Do you have a passion to learn? ').toUpperCase();
-//console.log(loveLearnign);
-if (loveLearnign === 'YES' || loveLearnign === 'Y') {
-  alert( loveLearnign +' great! I have passion too and I like to learn something new ' );
-} else if (loveLearnign === 'NO' || loveLearnign === 'N') {
-  alert(loveLearnign + ' Try to love it , it is so important ');
+var loveLearning = prompt('Do you know what I\'m studying know? ').toUpperCase();
+//console.log(loveLearning);
+if (loveLearning === 'YES' || loveLearning === 'Y') {
+// alert( loveLearning +' great! you are doing fine' );
+  score++;
+} else if (loveLearning === 'NO' || loveLearning === 'N') {
+  //alert(loveLearning + ' Not a problem ');
 } else {
   alert('Nothing');
 }
 
-var gameDevelopment = prompt('Do you know game-development ? ').toUpperCase();
-//console.log(gameDevelopment);
-if (gameDevelopment === 'YES' || gameDevelopment === 'Y') {
-  alert(gameDevelopment + ' Wow! like me , and it is my hobby' );
-} else if (gameDevelopment === 'NO' || gameDevelopment === 'N') {
-  alert(gameDevelopment + ' Try it , it is somthing cool ');
+var myHoppy = prompt('Do you know what is my hoppy ? ').toUpperCase();
+//console.log(myHoppy);
+if (myHoppy === 'YES' || myHoppy === 'Y') {
+// alert(myHoppy + ' Wow! you are amazing' );
+  score++;
+} else if (myHoppy === 'NO' || myHoppy === 'N') {
+// alert(myHoppy + ' sorry you are wrong');
 } else {
   alert('Nothing');
 }
 
-var workUnderPressur = prompt('Do you can work under pressure ? ').toUpperCase();
 
-switch (workUnderPressur) {
+
+var myAge = prompt('Do you know how old am I ? ').toUpperCase();
+//console.log(myAge);
+switch (myAge) {
 case 'YES':
-  alert(' Me too!');
-  break;
-case 'NO':
-  alert(' Sorry but this is  the reality,you should work under the pressure!');
-  break;
 case 'Y':
-  alert('Me too!');
+  //alert(' Yeaaah , you are right!');
+  score++;
   break;
+case 'NO': // like (or) on if statements
 case 'N':
-  alert(' Sorry but this is  the reality,you should work under the pressure!');
+  //alert(' it\'s wrong!');
   break;
 default:
   alert('nothing !');
   break;
 }
 
-alert('I hope to enjoy your time in my website');
 
+// var travelTo = prompt('Do you know how many countries I traveled to ? ').toUpperCase();
+
+
+for (let i = 0; i<= 3; i++) {
+  var travelNumber = parseInt(prompt('How many countries I traveled to ? you have just 4 attempt .Be careful'));
+  //console.log(travelNumber);
+  switch(travelNumber){
+  case 3:
+    alert(' True!');
+    score++;
+    i=4;
+    break;
+  case 1:
+  case 2:
+  case 4:
+  case 5:
+    alert(' Too High');
+
+    break;
+  case 6:
+  case 7:
+  case 8:
+  case 9:
+  case 10:
+    alert(' Too Low');
+
+    break;
+  default:
+    alert('nothing !');
+    i=4;
+    break;
+  }
+
+  if(i===3){
+    travelNumber=alert('The correct answer is : 3');
+  }
+}
+
+
+
+
+for(var j=0 ; j < 6 ; j++){
+  var favCountries = ['palestine' , 'turkey' , 'jordan' , 'italy'];
+  for(var x = 0; x < favCountries.length ; x = x+1){
+    var myFav = prompt('What\'s my favourite countries?').toLowerCase();
+    switch(myFav){
+    case favCountries[0] :
+      alert('yes you are True !');
+      score++;
+      j=6;
+      //console.log(x);
+      x=favCountries.length;
+      break;
+
+    case favCountries[1] :
+      alert('yes you are True !');
+      score++;
+      j=6;
+      x=favCountries.length;
+      break;
+    case favCountries[2] :
+      alert('yes you are True !');
+      score++;
+      j=6;
+      x=favCountries.length;
+      break;
+    case favCountries[3] :
+      alert('yes, True !');
+      score++;
+      j=6;
+      x=favCountries.length;
+      break;
+
+    default:
+      alert('False !');
+      x=favCountries.length;
+      break;
+    }
+  }
+}
+
+alert('your score is ' + score);
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # About-Me
 I am Samah Thweib and this is my website.
+
+### Welcome to my blog and having fun in guessing game
